f89a0d4ba8b4cafb0645ff6e32e627390f79e57d
Nuraiym <nabdirasulkyzy@gmail.com>
2022-10-17 13:39:13 +0500

    counter

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const Plus = () => {
+    setCount(count + 1);
+  };
+
+  const Minus = () => {
+
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="counter">
+      <h1>Counter</h1>
+      <div className="count">
+        <div>{count}</div>
+        <button onClick={Plus}>increase</button>
+        <button onClick={Minus}>decrease</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
